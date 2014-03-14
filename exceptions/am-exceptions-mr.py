c051cf6070a40f7107c0b5ca91edb02a6a5fe7cb
irvingreid <irving@cfrq.net>
2014-03-14 11:37:06 -0400

    Track missing end events along with exceptions

--- exceptions/am-exceptions-mr.py
+++ exceptions/am-exceptions-mr.py
@@ -9,16 +9,27 @@ from collections import defaultdict, Counter
 # Output of map pass is
 # "exception-text => count"
 def map(k, d, v, cx):
+    cx.write("Sessions", 1)
     j = json.loads(v)
 
     if not 'simpleMeasurements' in j:
-      return
+        return
+    failure = "NONE "
     s = j['simpleMeasurements']
+    if not 'AMI_startup_end' in s:
+        cx.write("No AMI_startup_end", 1)
+        failure = "AMI  "
+    if not 'XPI_bootstrap_addons_end' in s:
+        cx.write("No XPI_bootstrap_addons_end", 1)
+        failure = "BOOT "
+    if not 'XPI_startup_end' in s:
+        cx.write("No XPI_startup_end", 1)
+        failure = "XPI  "
     if not 'addonManager' in s:
         return
     a = s['addonManager']
     if 'exception' in a:
-        cx.write(json.dumps(a['exception']), 1)
+        cx.write(failure + json.dumps(a['exception']), 1)
 
 def reduce(k, v, cx):
     cx.write(k, sum(v))
