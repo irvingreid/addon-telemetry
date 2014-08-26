915d31c850c4bd72d579ad4e4d7fd6e3568333e3
irvingreid <irving@cfrq.net>
2014-08-26 14:48:14 -0400

    Track sync XPIDB reads by app/platform

--- sync-reads/xpi-sync-reads.py
+++ sync-reads/xpi-sync-reads.py
@@ -3,6 +3,7 @@
 
 import simplejson as json
 from collections import defaultdict, Counter
+import re
 
 measures = [
   'XPIDB_lateOpen_byType',
@@ -11,25 +12,55 @@ measures = [
   'XPIDB_lateOpen_updateActive',
   'XPIDB_lateOpen_writeList']
 
+section_regex = re.compile(r',"(?:info|addonDetails|slowSQL|ver|log|fileIOReports|histograms|lateWrites|addonHistograms|UIMeasurements|threadHangStats|simpleMeasurements|chromeHangs|slowSQLStartup)":|}$')
+# Extract a top level section out of the telemetry JSON packet by guessing at string boundaries
+def stringPart(j, section):
+  # Find the start of the requested section
+  startPattern = '"' + section + '":[[{]'
+  secmatch = re.search(startPattern, j)
+  if not secmatch:
+    return None
+  # Now find the first section start after that
+  endmatch = section_regex.search(j, secmatch.end())
+  if not endmatch:
+    print "Can't find an ending tag after", section, "in", j
+    return None
+  return j[secmatch.end() - 1 : endmatch.start()]
+
+def jsonPart(j, section):
+  sect = stringPart(j, section)
+  if sect:
+      return json.loads(sect)
+  return None
+
+# Assorted regexes to match fields we want from the telemetry data
+OS_regex = re.compile(r'"OS":"([^"]+)"')
+
 # d is filter criteria:
 # [reason, appName, appUpdateChannel, appVersion, appBuildID, submission_date]
 # Output of map pass is
-# "exception-text => count"
+# "(app, platform, measure, phase) => count"
 def map(k, d, v, cx):
-    cx.write("Sessions", 1)
-    j = json.loads(v)
+    reason, appName, appUpdateChannel, appVersion, appBuildID, submission_date = d
 
-    if not 'simpleMeasurements' in j:
-        cx.write("No simpleMeasurements", 1)
+    osm = OS_regex.search(v)
+    if osm:
+        os = osm.group(1)
+    else:
+        os = "?"
+
+    cx.write((appName, os, "Sessions", "."), 1)
+    s = jsonPart(v, 'simpleMeasurements')
+    if not s:
+        cx.write((appName, os, "No", "simpleMeasurements"), 1)
         return
-    s = j['simpleMeasurements']
     if not 'addonManager' in s:
-        cx.write("No addonManager", 1)
+        cx.write((appName, os, "No", "addonManager"), 1)
         return
     a = s['addonManager']
     for m in measures:
         if m in a:
-            cx.write(m + '-' + a[m], 1)
+            cx.write((appName, os, m, a[m]), 1)
 
 def reduce(k, v, cx):
     cx.write(k, sum(v))
